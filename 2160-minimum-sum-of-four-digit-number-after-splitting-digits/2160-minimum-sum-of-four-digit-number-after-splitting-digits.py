29f8e6bf2792320d093ee28044f45560111eb2fd
Aries298 <85286463+Aries298@users.noreply.github.com>
2022-10-06 15:14:58 +0200

    Time: 62 ms (18.37%), Space: 13.9 MB (10.59%) - LeetHub

--- 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.py
+++ 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.py
@@ -1,11 +1,5 @@
 from itertools import permutations
 class Solution:
     def minimumSum(self, num: int) -> int:
-        perms = list(permutations(str(num)))
-        pairs = [(x[0]+x[1], x[2]+x[3]) for x in perms]
-        lowest_sum = 1000
-        for pair in pairs:
-            pair_sum = int(pair[0]) + int(pair[1])
-            if pair_sum < lowest_sum:
-                lowest_sum = pair_sum
-        return lowest_sum
+        s = sorted(str(num))
+        return int(s[0]+s[2])+int(s[1]+s[3])
