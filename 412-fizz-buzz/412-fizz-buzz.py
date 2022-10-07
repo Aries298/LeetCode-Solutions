d2cc6133c11706164fc3922c9a4d72b580e08669
Aries298 <85286463+Aries298@users.noreply.github.com>
2022-10-07 15:46:35 +0200

    Time: 77 ms (49.84%), Space: 15.2 MB (17.41%) - LeetHub

--- 412-fizz-buzz/412-fizz-buzz.py
+++ 412-fizz-buzz/412-fizz-buzz.py
@@ -1,9 +1,3 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
-        ans = []
-        for i in range (1,n+1):
-            if i % 3 == 0 or i % 5 == 0:
-                ans.append((i % 3 == 0) * 'Fizz' + (i % 5 == 0) * 'Buzz')
-            else:
-                ans.append(str(i))
-        return ans
+        return [(i % 3 == 0) * 'Fizz' + (i % 5 == 0) * 'Buzz' if (i % 3 == 0 or i % 5 == 0) else str(i) for i in range(1,n+1)]
