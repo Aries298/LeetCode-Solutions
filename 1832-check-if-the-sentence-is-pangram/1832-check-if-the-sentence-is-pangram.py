0f98cd6bacc89f45ffeb7d49f853520f00545128
Aries298 <85286463+Aries298@users.noreply.github.com>
2022-10-17 21:34:11 +0200

    Time: 62 ms (33.33%), Space: 13.9 MB (54.73%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
@@ -0,0 +1,34 @@
+class Solution:
+    def checkIfPangram(self, sentence: str) -> bool:
+        dic = {
+    "a":0,   
+    "b":0,   
+    "c":0,   
+    "d":0,   
+    "e":0,   
+    "f":0,   
+    "h":0,   
+    "i":0,
+    "j":0,
+    "g":0,   
+    "k":0,   
+    "l":0,   
+    "m":0,   
+    "n":0,   
+    "o":0,   
+    "p":0,   
+    "q":0,   
+    "r":0,
+    "s":0,  
+    "t":0,   
+    "u":0,   
+    "v":0,   
+    "w":0,   
+    "x":0,
+    "y":0,
+    "z":0,
+}
+        for letter in sentence:
+            dic[letter] += 1
+        print(dic)
+        return 0 not in dic.values()
