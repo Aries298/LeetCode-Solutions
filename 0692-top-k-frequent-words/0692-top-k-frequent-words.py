9b417f1cd6c95096060b860ef6e811d095f7947b
Aries298 <85286463+Aries298@users.noreply.github.com>
2022-10-19 22:02:54 +0200

    Time: 112 ms (53.30%), Space: 14 MB (64.57%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -1,9 +1,3 @@
-from collections import Counter
-
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        c = sorted(words)
-        c = Counter(c)
-        c = c.most_common(k)
-        c = [x[0] for x in c]
-        return c
+        return [x[0] for x in Counter(sorted(words)).most_common(k)]
