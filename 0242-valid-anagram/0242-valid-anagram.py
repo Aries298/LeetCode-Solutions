807b4f946d179b165e2276a187f334db1eb3144e
Aries298 <85286463+Aries298@users.noreply.github.com>
2022-10-28 12:25:45 +0200

    Time: 52 ms (92.32%), Space: 14.5 MB (34.93%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -2,7 +2,10 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
+        seen = set()
         for letter in s:
-            if s.count(letter) != t.count(letter):
-                return False
+            if letter not in seen:
+                seen.add(letter)
+                if s.count(letter) != t.count(letter):
+                    return False
         return True
