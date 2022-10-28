b1f30e3a392099c418a72c9d777f5a9355c2ac9c
Aries298 <85286463+Aries298@users.noreply.github.com>
2022-10-28 12:23:51 +0200

    Time: 4406 ms (5.01%), Space: 14.4 MB (67.10%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,3 +1,8 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        if len(s) != len(t):
+            return False
+        for letter in s:
+            if s.count(letter) != t.count(letter):
+                return False
+        return True
