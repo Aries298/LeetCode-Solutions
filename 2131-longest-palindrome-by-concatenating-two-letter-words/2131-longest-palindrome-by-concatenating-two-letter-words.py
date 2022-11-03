902af64c98394ba8ece05894741fc70c53b0c6a8
Aries298 <85286463+Aries298@users.noreply.github.com>
2022-11-03 16:17:40 +0100

    Time: 3237 ms (42.03%), Space: 38.6 MB (6.56%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.py
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.py
@@ -1,9 +1,7 @@
 class Solution:
     def longestPalindrome(self, words: List[str]) -> int:
         ans = 0
-        words = dict(Counter(words))
-        words = [[k, v] for k, v in words.items()]
-        print(words)
+        words = [[k, v] for k, v in (dict(Counter(words))).items()]
         for i in range(len(words)-1):
             for j in range(i+1,len(words)):
                 if words[i][0] == words[j][0][::-1]:
@@ -11,21 +9,12 @@ class Solution:
                     ans += 2 * count * len(words[i][0])
                     words[i][1] -= count
                     words[j][1] -= count
-
-        print(words)
-
         words = [x for x in words if len(set(x[0])) == 1]
-
-        print(words)
-
         for i in range(len(words)):
             if words[i][1]:
                 count = words[i][1] // 2
                 words[i][1] %= 2
                 ans += 2 * count * len(words[i][0])
-        print(words)
-
         max_single_length = max([len(x) for x in words if len(set(x[0])) == 1 and x[1] > 0], default=0)
-        print(max_single_length)
         ans += max_single_length
         return ans
