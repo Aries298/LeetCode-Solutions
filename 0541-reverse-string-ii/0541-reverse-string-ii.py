655bc717641d842be146e77459892775896f06e4
Aries298 <85286463+Aries298@users.noreply.github.com>
2022-11-04 14:46:26 +0100

    Time: 72 ms (26.95%), Space: 14 MB (59.06%) - LeetHub

--- 0541-reverse-string-ii/0541-reverse-string-ii.py
+++ 0541-reverse-string-ii/0541-reverse-string-ii.py
@@ -1,11 +1,9 @@
 from math import ceil
 class Solution:
     def reverseStr(self, s: str, k: int) -> str:
-        ans = ''
-        
-        for i in range(ceil(len(s)/(2*k))):
-            curr = s[i*(2*k):i*(2*k)+(2*k)]
-            s1 = curr[:k]
-            s2 = curr[k:]
+        ans, size = '', 2 * k
+        for i in range(ceil(len(s)/size)):
+            curr = s[i*size:i*size+size]
+            s1, s2 = curr[:k], curr[k:] 
             ans += ''.join(list(reversed(s1)))+s2
         return ans
