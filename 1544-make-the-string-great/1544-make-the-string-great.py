e0c0263ad3fe9ecd9bb2b9fbd3b04679ae641fcf
Aries298 <85286463+Aries298@users.noreply.github.com>
2022-11-08 18:33:13 +0100

    Time: 114 ms (5.13%), Space: 13.8 MB (61.93%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.py
+++ 1544-make-the-string-great/1544-make-the-string-great.py
@@ -6,15 +6,12 @@ class Solution:
                 prev = s[0]
             else:
                 return s
-            
             for i in range(1, len(s)):
                 curr = s[i]
                 if curr != prev and curr.lower() == prev.lower() and i-1 not in to_del:
-                    to_del.append(i-1)
-                    to_del.append(i)
+                    to_del.extend([i-1, i])
                 prev = curr
-            s = [letter for i,letter in enumerate(s) if i not in to_del]
-            s = ''.join(s)
+            s = ''.join([letter for i,letter in enumerate(s) if i not in to_del])
             if not to_del:
                 break
         return s
