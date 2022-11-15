4bfdbaf5c54eba7316cb3141b8aa9f3fc890f3ab
Aries298 <85286463+Aries298@users.noreply.github.com>
2022-11-15 17:51:41 +0100

    Time: 58 ms (38.85%), Space: 13.8 MB (58.55%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.py
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.py
@@ -1,4 +1,3 @@
 class Solution:
-    @cache
     def sortSentence(self, s: str) -> str:
         return ' '.join(dict(sorted((dict(zip([word[-1] for word in s.split()], [word[:-1] for word in s.split()]))).items())).values())
