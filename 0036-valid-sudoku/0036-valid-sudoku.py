1df986868d9e344dfac87b0831d9c990bc2e6be2
Aries298 <85286463+Aries298@users.noreply.github.com>
2022-11-23 16:03:39 +0100

    Time: 267 ms (14.74%), Space: 13.8 MB (83.37%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,3 +1,3 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        return False not in [len(nums) == len(set(nums)) for nums in [[board[(i*3)+n][(j*3)+m] for n in range(3) for m in range(3) if board[(i*3)+n][(j*3)+m] != "."] for j in range(3) for i in range(3)]] + [len(nums) == len(set(nums)) for nums in [[board[j][i] for j in range(9) if board[j][i] != "."] for i in range(9)]] + [len(nums) == len(set(nums)) for nums in [[el for el in board[i] if el != "."] for i in range(9)]]
+        return not sum([len(nums) != len(set(nums)) for nums in [[board[(i*3)+n][(j*3)+m] for n in range(3) for m in range(3) if board[(i*3)+n][(j*3)+m] != "."] for j in range(3) for i in range(3)]] + [len(nums) != len(set(nums)) for nums in [[board[j][i] for j in range(9) if board[j][i] != "."] for i in range(9)]] + [len(nums) != len(set(nums)) for nums in [[el for el in board[i] if el != "."] for i in range(9)]])
