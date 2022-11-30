a9aa6872343320507008b56ca6ec48035c30e06f
Aries298 <85286463+Aries298@users.noreply.github.com>
2022-11-30 11:51:05 +0100

    Time: 450 ms (5.07%), Space: 32.3 MB (6.64%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,6 +1,7 @@
 import numpy as np
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        for num in nums2:
-            bisect.insort(nums1, num)
-        return np.median(nums1)
+        (bigger, smaller) = (nums1, nums2) if len(nums1) > len(nums2) else (nums2, nums1)
+        for num in smaller:
+            bisect.insort(bigger, num)
+        return np.median(bigger)
