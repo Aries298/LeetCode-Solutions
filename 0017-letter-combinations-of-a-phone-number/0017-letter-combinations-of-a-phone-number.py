885e74b77d422111e8dc8ad39deb906fc24ea2c4
Aries298 <85286463+Aries298@users.noreply.github.com>
2022-12-01 13:25:34 +0100

    Time: 57 ms (50.11%), Space: 13.9 MB (80.27%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,33 @@
+digits_dict = {
+    '2': 'abc',
+    '3': 'def',
+    '4': 'ghi',
+    '5': 'jkl',
+    '6': 'mno',
+    '7': 'pqrs',
+    '8': 'tuv',
+    '9': 'wxyz',
+}
+
+
+
+
+class Solution:
+    def letterCombinations(self, digits: str):
+        digits = list(digits)
+        digits = reversed([digits_dict[num] for num in digits])
+        ans = []
+        for letter_set in digits:
+            new_list = []
+            for letter in letter_set:
+                added = False
+                if ans:
+                    for letter_set in ans:
+                        new_list.append(letter + letter_set)
+                else:
+                    added = True
+                    ans = list(letter_set)
+                    break
+            if not added:
+                ans = new_list
+        return ans
