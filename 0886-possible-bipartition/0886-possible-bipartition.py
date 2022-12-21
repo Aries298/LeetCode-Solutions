9302f31a04e191309e6f5eea41d128e8ac78fc53
Aries298 <85286463+Aries298@users.noreply.github.com>
2022-12-21 11:59:49 +0100

    Time: 1894 ms (35.09%), Space: 24 MB (6.05%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.py
+++ 0886-possible-bipartition/0886-possible-bipartition.py
@@ -1,14 +1,19 @@
 class Solution:
     def possibleBipartition(self, N, dislikes):
-        def visit(a, depth):     
-            if a in depths:
-                return (depth - depths[a]) % 2 == 0
-            depths[a] = depth
-            return all(visit(b, depth + 1) for b in d[a])
-
-        d = collections.defaultdict(set)
-        depths = {}
+        d = defaultdict(set)
         for a,b in dislikes:
             d[a].add(b)
             d[b].add(a)
-        return all(a in depths or visit(a, 0) for a in d)
+        group = {i:None for i in range(1, N+1)}
+        
+        def dfs(node, g):
+            if not group[node]: group[node] = g
+            else: return group[node] == g
+            
+            for peeps in d[node]:
+                if not dfs(peeps, 2 if g == 1 else 1): return False
+            return True
+        
+        for n in range(1,N+1):
+            if not group[n] and not dfs(n,1): return False
+        return True
