250d0d3a706d575a49a8b3f09577e9786cd8a6c3
Aries298 <85286463+Aries298@users.noreply.github.com>
2022-12-21 13:52:35 +0100

    Time: 857 ms (72.66%), Space: 23 MB (5.07%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
@@ -1,14 +1,10 @@
 class Solution:
     def equalPairs(self, grid: List[List[int]]) -> int:
-        cols = []
         rows = [row for row in grid]
-        for i in range(len(rows)): cols.append([row[i] for row in rows])
-
+        cols = [[row[i] for row in rows] for i in range(len(rows))]
         intersection = set()
-
         for i, row in enumerate(rows):
             for j, col in enumerate(cols):
-                if row == col:
-                    intersection.add((i,j))
+                if row == col: intersection.add((i,j))
         return len(intersection)
 
