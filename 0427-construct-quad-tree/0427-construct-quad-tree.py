9f5e00aa0ea7ff9638008a30fa5bd68e96809b77
Aries298 <85286463+Aries298@users.noreply.github.com>
2023-02-27 11:22:11 +0100

    Time: 104 ms (96.14%), Space: 14.8 MB (41.32%) - LeetHub

---
+++ 0427-construct-quad-tree/0427-construct-quad-tree.py
@@ -0,0 +1,11 @@
+class Solution:
+    def construct(self, grid):
+        def dfs(x = 0, y = 0, n = len(grid)):                                             
+            if all (grid[i+x][j+y] == grid[x][y]           
+                    for i in range(n) for j in range(n)):
+                return Node(grid[x][y] == 1, True)
+            n//= 2                                              
+            return Node(False, False,                  
+                        dfs(x,y,n),     dfs(x,y+n,n), 
+                        dfs(x+n,y,n), dfs(x+n,y+n,n))
+        return dfs()                             
