7b95261532c9998e082451f6d68a10e500ef7391
Aries298 <85286463+Aries298@users.noreply.github.com>
2023-04-08 07:51:26 +0200

    Time: 39 ms (70.39%), Space: 14.4 MB (17.48%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.py
@@ -0,0 +1,19 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node_: 'Node') -> 'Node':
+        copies = {None: None}
+
+        def deep_copy(node: 'Node') -> 'Node':
+            if node in copies: return copies[node]
+            copies[node] = c_node = Node(node.val)
+            c_node.neighbors = list(map(deep_copy, node.neighbors))
+            return c_node
+        
+        return deep_copy(node_)
