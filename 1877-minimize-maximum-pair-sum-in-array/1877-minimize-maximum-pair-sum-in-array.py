ec9cf6ac35eb3c59bef78d0c88b4b8ccde2a6f74
Aries298 <85286463+Aries298@users.noreply.github.com>
2023-11-17 07:26:36 +0100

    Time: 1004 ms (33.93%), Space: 30.3 MB (78.66%) - LeetHub

--- 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.py
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.py
@@ -1,6 +1,3 @@
 class Solution:
     def minPairSum(self, nums: List[int]) -> int:
-        nums = sorted(nums)
-        ans = 0
-        first, second = nums[:int(len(nums)/2)], nums[int(len(nums)/2):][::-1]
-        return max([a+b for a,b in zip(first,second)])
+        return nums.sort() or max((nums[i]+nums[-(i+1)]) for i in range(len(nums)//2+1))
