3ffb4ec85137238c94ac05c6c6415e06764a71bd
Aries298 <85286463+Aries298@users.noreply.github.com>
2023-12-16 09:10:26 +0100

    Time: 61 ms (22.46%), Space: 17.5 MB (19.13%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,11 +1,5 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-        seen = set()
-        for letter in s:
-            if letter not in seen:
-                seen.add(letter)
-                if s.count(letter) != t.count(letter):
-                    return False
-        return True
+        sorted_s = sorted(s)
+        sorted_t = sorted(t)
+        return sorted_s == sorted_t
