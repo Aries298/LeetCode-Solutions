55c3603e45e69432f6e96db462abf8b2419c352e
Aries298 <85286463+Aries298@users.noreply.github.com>
2023-12-29 08:14:35 +0100

    Time: 1309 ms (20.35%), Space: 17.5 MB (55.85%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.py
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.py
@@ -1,14 +1,33 @@
 class Solution:
-    def minDifficulty(self, jobDifficulty: List[int], d: int) -> int:
-        @cache
-        def helper(intended, end):
-            if intended == 1:
-                 return max(jobDifficulty[:end])
-            res, mx = math.inf, 0
-            for done in range(1, end-intended+2):
-                mx = max(mx, jobDifficulty[end-done])
-                res = min(helper(intended-1, end-done) + mx, res)
-            return res
-        return helper(d, len(jobDifficulty)) if len(jobDifficulty) >= d else -1
-        
-    
+    def minDifficulty(self, jobDifficulty, days):
+        length = len(jobDifficulty)
+        if days > length:
+            return -1
+
+        min_difficulties = [[float('inf')] * length for _ in range(days)]
+
+        max_diff = 0
+        i = 0
+        while i <= length - days:
+            max_diff = max(max_diff, jobDifficulty[i])
+            min_difficulties[0][i] = max_diff
+            i += 1
+
+        current_day = 1
+        while current_day < days:
+            to = current_day
+            while to <= length - days + current_day:
+                current_job_difficulty = jobDifficulty[to]
+                result = float('inf')
+                j = to - 1
+                while j >= current_day - 1:
+                    result = min(result, min_difficulties[current_day - 1][j] + current_job_difficulty)
+                    current_job_difficulty = max(current_job_difficulty, jobDifficulty[j])
+                    j -= 1
+                min_difficulties[current_day][to] = result
+                to += 1
+            current_day += 1
+
+        return min_difficulties[days - 1][length - 1]
+
+
