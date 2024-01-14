2192a77818a79b8eee655cdbea6ec720c97aa4df
Aries298 <85286463+Aries298@users.noreply.github.com>
2024-01-14 09:53:18 +0100

    Time: 339 ms (6.14%), Space: 21.8 MB (5.26%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
@@ -1,4 +1,27 @@
+import collections
+
 class Solution:
     def closeStrings(self, word1: str, word2: str) -> bool:
-        return set(word1) == set(word2) and Counter(Counter(word1).values()) == Counter(Counter(word2).values())
+        if len(word1) != len(word2):
+            return False
+        diff = []
+        spectrum = {}
+        spectrum2 = {}
+        for index in range(len(word1)):
+            spectrum[word1[index]] = spectrum.get(word1[index], 0) + 1
+            spectrum2[word2[index]] = spectrum2.get(word2[index], 0) + 1
+            if word1[index] != word2[index]:
+                diff.append(index)
+        if len(diff) == 2:
+            if word1[diff[0]] == word2[diff[1]] and word1[diff[1]] == word2[diff[0]]:
+                return True
+            return False
+        else:
+            if set(word1) != set(word2):
+                return False
+            count1, count2 = collections.Counter(word1), collections.Counter(word2)
+            return sorted(count1.values()) == sorted(count2.values())
+        
+        
+
         
