fe5e140c5582fab51663a43c35a0d65301bc6d98
Aries298 <85286463+Aries298@users.noreply.github.com>
2024-01-15 07:40:37 +0100

    Time: 1538 ms (57.39%), Space: 71.2 MB (66.20%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
@@ -1,10 +1,18 @@
+from collections import defaultdict
 class Solution:
     def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
-        players = set()
+        dct=defaultdict(lambda :[0,0])
         for p1,p2 in matches:
-            players.add(p1)
-            players.add(p2)
-        losses = dict(zip(players, [0] * len(players)))
-        for match in matches:
-            losses[match[1]] += 1
-        return [sorted([num for num, losses in losses.items() if losses == 0]), sorted([num for num, losses in losses.items() if losses == 1])]
+            dct[p1][0]+=1
+            dct[p1][1]+=1
+            dct[p2][0]+=1
+        wonAll=[]
+        lossOne=[]
+        for i in dct:
+            if dct[i][0]==dct[i][1]:
+                wonAll.append(i)
+            elif dct[i][0]-dct[i][1]==1:
+                lossOne.append(i)
+        wonAll.sort()
+        lossOne.sort()
+        return [wonAll,lossOne]
