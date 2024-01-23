c73010f692c890e217a74633f16a3281225d4a03
Aries298 <85286463+Aries298@users.noreply.github.com>
2024-01-23 08:02:54 +0100

    Time: 55 ms (98.57%), Space: 19 MB (31.19%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
@@ -1,20 +1,21 @@
-import itertools
-
 class Solution:
     def maxLength(self, arr: List[str]) -> int:
-        combs = []
-        for L in range(len(arr) + 1):
-            for subset in itertools.combinations(arr, L):
-                combs.append(subset)
-
-        ans = 0
-        for comb in combs:
-            tmp = ""
-            for el in comb:
-                tmp += el
-            if len(tmp) == len(set(tmp)):
-                ans = len(tmp) if ans < len(tmp) else ans
-                
-        return ans
+        dp = [0]
+        res = 0
         
-       
+        for s in arr:
+            a, dup = 0, 0
+            for c in s:
+                dup |= a & (1 << (ord(c) - ord('a')))
+                a |= 1 << (ord(c) - ord('a'))
+            
+            if dup > 0:
+                continue
+            
+            for i in range(len(dp) - 1, -1, -1):
+                if (dp[i] & a) > 0:
+                    continue
+                dp.append(dp[i] | a)
+                res = max(res, bin(dp[i] | a).count('1'))
+        
+        return res
