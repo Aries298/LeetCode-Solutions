d7718d301a1924f336493072df5989b286dc9935
Aries298 <85286463+Aries298@users.noreply.github.com>
2024-02-07 07:55:19 +0100

    Time: 58 ms (27.94%), Space: 19.7 MB (18.31%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -1,8 +1,3 @@
 class Solution:
     def frequencySort(self, s: str) -> str:
-        ans = []
-        c = Counter(s).most_common()
-        for pair in c:
-            for i in range(pair[1]):
-                ans.append(pair[0])
-        return ans
+        return [pair[0] for pair in Counter(s).most_common() for i in range(pair[1])]
