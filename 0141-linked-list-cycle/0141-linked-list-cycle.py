dab4755a6e935fcf3e87757b3f942b4040f4ffad
Aries298 <85286463+Aries298@users.noreply.github.com>
2024-03-06 08:00:53 +0100

    Time: 34 ms (97.94%), Space: 19 MB (79.81%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -1,17 +1,10 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
-class Solution(object):
-    def hasCycle(self, head):
-        # Initialize two node slow and fast point to the hesd node...
-        fastptr = head
-        slowptr = head
-        while fastptr is not None and fastptr.next is not None:
-            slowptr = slowptr.next
-            fastptr = fastptr.next.next
-            if slowptr == fastptr:
-                return 1
-        return 0
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        fast = slow = head
+        
+        while fast and fast.next:
+            slow, fast = slow.next, fast.next.next
+            if fast == slow:
+                return True
+        
+        return False
