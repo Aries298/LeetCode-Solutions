6d6705d6e1deb9f014b7282ed0da67837e4ad88a
Aries298 <85286463+Aries298@users.noreply.github.com>
2024-03-26 17:37:48 +0100

    Time: 263 ms (96.79%), Space: 37.8 MB (5.31%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -1,9 +1,7 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        maximum = max(nums)
         diff = (set(range(1,len(nums)+1))).difference(set(nums))
         if diff:
             return min(diff)
         else:
-            return maximum + 1
-        
+            return max(nums) + 1
