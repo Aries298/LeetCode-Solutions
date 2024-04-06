83ef8e8ac3e56d8100784fa15c8511173ff4ed1a
Aries298 <85286463+Aries298@users.noreply.github.com>
2024-04-06 08:57:24 +0200

    Time: 85 ms (43.25%), Space: 19 MB (17.04%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
@@ -0,0 +1,26 @@
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        # Initialize variables
+        openParenthesesCount = 0
+        arr = list(s)
+
+        # First pass: mark excess closing parentheses with '*'
+        for i in range(len(arr)):
+            if arr[i] == '(':
+                openParenthesesCount += 1
+            elif arr[i] == ')':
+                if openParenthesesCount == 0:
+                    arr[i] = '*' # Mark excess closing parentheses
+                else:
+                    openParenthesesCount -= 1
+
+        # Second pass: mark excess opening parentheses from the end
+        for i in range(len(arr) - 1, -1, -1):
+            if openParenthesesCount > 0 and arr[i] == '(':
+                arr[i] = '*' # Mark excess opening parentheses
+                openParenthesesCount -= 1
+        
+        # Filter out marked characters and construct the result string
+        result = ''.join(c for c in arr if c != '*')
+
+        return result
